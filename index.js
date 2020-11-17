bcb639c2c092ec6696f9e16ff7cc08d3f4234a16
boris Baganov <bbaganov@gmail.com>
2020-11-17 16:52:40 +0300

    Add remove string

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
+let crossButtonsAll = document.querySelectorAll('.input_cross_button');
 const addButton = document.querySelector('.conteiner_add_button');
+let num = 1;
 
 addButton.addEventListener('click',(event)=>{
     event.preventDefault();
-
+    
     const inputDiv = document.querySelector('.input');
     const conteinerInput = document.querySelector('.conteiner_input');
     const innerInput = inputDiv.innerHTML;
     const divInput = document.createElement('div');
     divInput.classList.add('input');
+    divInput.setAttribute("id", num);
     divInput.innerHTML = innerInput;
     conteinerInput.append(divInput);
     console.log(divInput);
+    num +=1; 
+    
+    console.log(crossButtonsAll);
+    crossButtonsAll = document.querySelectorAll('.input_cross');
+
+    crossButtonsAll.forEach((crossButton, index, array) => {
+        console.log(crossButton);
+        crossButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            console.log(event);
+            crossButton.parentElement.remove();
+    
+        })
+    })
+
+    
+
 });
 
 const sortButton = document.querySelector('.roll-button');
 sortButton.addEventListener('click', (event)=>{
     event.preventDefault();
+    console.log(event);
 
     const allInput = document.querySelectorAll('.input_string');
     console.log(allInput);
@@ -63,9 +84,10 @@ sortButton.addEventListener('click', (event)=>{
     allInputString.forEach((inputString, index, array) =>{
         inputString.value = inputData[index];
     })
-
-
-
 });
+
+
+console.log(crossButtonsAll);
+
 
 
